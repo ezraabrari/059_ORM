b271685abd8600f2536d7a3dbc05f8a2cbfba268
Ezra Abrari <ezraabrari@gmail.com>
2025-10-27 10:00:20 +0700

    menambahkan endpoint DELETE untuk menghapus data komik

--- index.js
+++ index.js
@@ -58,3 +58,18 @@ app.put('/komik', async (req, res) => {
         res.status(500).send(err);
     }
 });
+
+app.delete('/komik', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const komik = await db.komik.findByPk(id);
+        if(!komik) {
+            return res.statur(404).send({ message: "komik tidak ditemukan"});
+        }
+
+        await komik.destroy();
+        res.send({ message : "komik berhasil dihapus"});
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
