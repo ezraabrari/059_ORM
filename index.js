2b24323a5e170ebb53fa79de3bf97bcb167c3bea
Ezra Abrari <ezraabrari@gmail.com>
2025-10-27 09:58:54 +0700

    menambahkan endpoint POST untuk menambah data komik

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const PORT = 3000;
 const db = require("./models");
+const res = require('express/lib/response');
 app.use(express.json());
 app.use(express.urlencoded({
     extended:false
@@ -20,3 +21,13 @@ db.sequelize.sync()
     .catch((err) => {
         console.log(err);
     })
+
+app.post("/komik", async (req, res) =>{
+    const data = req.body;
+    try {
+        const komik = await db.komik.create(data);
+        res.send(komik);
+    }catch (err) {
+        res.send(err);
+    }
+});
